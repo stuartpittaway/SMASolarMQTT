e569da2223d812c5912dc8d62669f534b870cb28
Stuart Pittaway <stuart@binarycreations.co.uk>
2015-06-29 21:51:50 +0100

    Code tidy

--- SMASolarMQTT.py
+++ SMASolarMQTT.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-# Run this script like this...
-# This will background the task 
-# nohup python SMASolarMQTT.py 00:80:25:1D:AC:53 0000 1> spotvalues.csv 2> btscanerrors.log &
+# This will background the task
+# nohup python SMASolarMQTT.py 00:80:25:1D:AC:53 0000 1> SMASolarMQTT.log 2> SMASolarMQTT.log.error &
 
 __author__ = 'Stuart Pittaway'
 
@@ -21,20 +20,15 @@ import SMASolarMQTT_library
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
-
+    print("Connected to MQTT with result code "+str(rc))
+    pass
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    # print(msg.topic+" "+str(msg.payload))
-    # do nothing
+    #print(msg.topic+" "+str(msg.payload))
     pass
 
-
-def main(bd_addr, InverterPassword):
+def main(bd_addr, InverterPassword, mqtt_initial_node):
     InverterCodeArray = bytearray([0x5c, 0xaf, 0xf0, 0x1d, 0x50, 0x00]);
 
     # Dummy arrays
@@ -93,18 +87,26 @@ def main(bd_addr, InverterPassword):
                                                            AddressFFFFFFFF)
             packet_send_counter += 1
 
+
+            # Format the MQTT topics to publish values under
+            mqtt_prefix = "emonhub/rx"
+            topic_spotvalues_ac = "{0}/{1}/values".format(mqtt_prefix, mqtt_initial_node)
+            topic_spotvalues_dcwatts = "{0}/{1}/values".format(mqtt_prefix, mqtt_initial_node + 1)
+            topic_spotvalues_yield = "{0}/{1}/values".format(mqtt_prefix, mqtt_initial_node + 2)
+            topic_spotvalues_dc = "{0}/{1}/values".format(mqtt_prefix, mqtt_initial_node + 3)
+
+            # Connect to MQTT
             client = mqtt.Client()
             client.on_connect = on_connect
             client.on_message = on_message
             client.connect("localhost", 1883, 60)
 
             while True:
-
                 # MQTT Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
                 client.loop()
 
-                # Make sure the packet counter wont exceed 8 bits
-                if packet_send_counter > 200:
+                # Make sure the packet counter won't exceed 8 bits
+                if packet_send_counter > 222:
                     packet_send_counter = 0
 
                 L2 = SMASolarMQTT_library.spotvalues_ac(btSocket, packet_send_counter, mylocalBTAddress,
@@ -112,12 +114,13 @@ def main(bd_addr, InverterPassword):
                                                         AddressFFFFFFFF)
                 packet_send_counter += 1
 
+                # Output 10 parameters for AC values
                 payload = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}".format(L2[1][1], L2[2][1], L2[3][1], L2[4][1],
                                                                            L2[5][1],
                                                                            L2[6][1], L2[7][1], L2[8][1], L2[9][1],
                                                                            L2[10][1])
-                client.publish("emonhub/rx/50/values", payload=payload, qos=0, retain=False)
-                SMASolarMQTT_library.time.sleep(1)
+                client.publish(topic_spotvalues_ac, payload=payload, qos=0, retain=False)
+                time.sleep(1)
 
                 # print "dc watts"
                 L2 = SMASolarMQTT_library.spotvalues_dcwatts(btSocket, packet_send_counter, mylocalBTAddress,
@@ -125,30 +128,35 @@ def main(bd_addr, InverterPassword):
                                                              AddressFFFFFFFF)
                 packet_send_counter += 1
                 payload = "{0}".format(L2[1][1])
-                client.publish("emonhub/rx/52/values", payload=payload, qos=0, retain=False)
-                SMASolarMQTT_library.time.sleep(1)
+                client.publish(topic_spotvalues_dcwatts, payload=payload, qos=0, retain=False)
+                time.sleep(1)
 
-                # print "dc"
-                # These values only updated every 5 mins
-                L2 = SMASolarMQTT_library.spotvalues_dc(btSocket, packet_send_counter, mylocalBTAddress,
-                                                        InverterCodeArray,
-                                                        AddressFFFFFFFF)
-                packet_send_counter += 1
-                payload = "{0},{1}".format(L2[1][1], L2[2][1])
-                client.publish("emonhub/rx/51/values", payload=payload, qos=0, retain=False)
-                SMASolarMQTT_library.time.sleep(1)
+                if (packet_send_counter % 5==0):
+                    # Only run this function every 5 packets as its a slowly changing total number
+                    # print "yield"
+                    L2 = SMASolarMQTT_library.spotvalues_yield(btSocket, packet_send_counter, mylocalBTAddress,
+                                                               InverterCodeArray,
+                                                               AddressFFFFFFFF)
+                    packet_send_counter += 1
+                    payload = "{0},{1},{2},{3}".format(L2[1][1], L2[2][1], L2[3][1], L2[4][1])
+                    client.publish(topic_spotvalues_yield, payload=payload, qos=0, retain=False)
+                    time.sleep(1)
 
-                # print "yield"
-                L2 = SMASolarMQTT_library.spotvalues_yield(btSocket, packet_send_counter, mylocalBTAddress,
-                                                           InverterCodeArray,
-                                                           AddressFFFFFFFF)
-                packet_send_counter += 1
-                payload = "{0},{1},{2},{3}".format(L2[1][1], L2[2][1], L2[3][1], L2[4][1])
-                client.publish("emonhub/rx/53/values", payload=payload, qos=0, retain=False)
-                SMASolarMQTT_library.time.sleep(1)
+                if (packet_send_counter % 25==0):
+                    #Only run this function every 25 packets as this figure only updates every 5 minutes
+                    # print "dc"
+                    # These values only update every 5 mins by the inverter.
+                    L2 = SMASolarMQTT_library.spotvalues_dc(btSocket, packet_send_counter, mylocalBTAddress,
+                                                            InverterCodeArray,
+                                                            AddressFFFFFFFF)
+                    packet_send_counter += 1
+                    payload = "{0},{1}".format(L2[1][1], L2[2][1])
+                    client.publish(topic_spotvalues_dc, payload=payload, qos=0, retain=False)
+                    time.sleep(1)
 
-                # Time in seconds between readings - about 60 seconds is more than enough!
-                SMASolarMQTT_library.time.sleep(5)
+
+                # Delay to give PI some CPU back and avoid drowning the SMA inverter with BT traffic
+                time.sleep(5)
 
         except bluetooth.btcommon.BluetoothError as inst:
             print >> sys.stderr, "Bluetooth Error"
@@ -159,7 +167,7 @@ def main(bd_addr, InverterPassword):
 
             btSocket.close()
             # Give bluetooth time to recover
-            SMASolarMQTT_library.time.sleep(20)
+            time.sleep(20)
 
         except Exception as inst:
             # print >>sys.stderr, type(inst)     # the exception instance
@@ -168,11 +176,11 @@ def main(bd_addr, InverterPassword):
             traceback.print_exc(file=sys.stderr)
 
             btSocket.close()
-            SMASolarMQTT_library.time.sleep(20)
+            time.sleep(20)
 
 
 parser = argparse.ArgumentParser(
-    description='Report generation statistics from SMA photovoltaic inverter over a shared MQTT hub.',
+    description='Report generation statistics from SMA photovoltaic inverter over a shared MQTT hub.  Designed for use with emonCMS and emonPi see http://openenergymonitor.org/emon/',
     epilog='Copyright 2013-2015 Stuart Pittaway.')
 
 parser.add_argument('addr', metavar='addr', type=str,
@@ -181,8 +189,11 @@ parser.add_argument('addr', metavar='addr', type=str,
 parser.add_argument('passcode', metavar='passcode', type=str,
                     help='NUMERIC pass code for the inverter, default of 0000.')
 
+parser.add_argument('mqttinitialnode', metavar='mqttinitialnode', type=int,
+                    help='Initial emoncms NODE number to publish as using MQTT.')
+
 args = parser.parse_args()
 
-main(args.addr, args.passcode)
+main(args.addr, args.passcode, args.mqttinitialnode)
 
 exit()
